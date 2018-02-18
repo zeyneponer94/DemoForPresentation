08dcc2b21997d5bc8212f23b8b41123e459c6f1b
Zeynep Öner <Zeynep@Zeyneps-MacBook-Pro.local>
2018-02-18 12:46:14 +0300

    salesforce mapping is done

---
+++ dbOperations.js
@@ -0,0 +1,30 @@
+module.exports = {
+    
+    
+    
+    dbChanges : function(req, res){
+        
+
+
+        var pg = require('pg');
+        
+        const conString = 'postgres://krndjquidnhbml:c34245278e28a697031fd314dd590e189c8967dd10428e946fd0327a356e13fa@ec2-54-163-234-99.compute-1.amazonaws.com:5432/dcf0qi2krf1gls';
+        
+        var client = new pg.Client(conString);
+        client.connect();
+        
+        
+        var query = client.query("select (id,name) from salesforce.Account ");
+        
+        query.on('row', function(row) {
+                 query = client.query("insert into demo(id, name) " + "values ("+row.id+",'"+row.name+"')");
+                 res.sendStatus(200);
+         });
+        
+        
+    }
+
+};
+
+
+
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+var dbOperations = require("./dbOperations.js");
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', (req, res) => dbOperations.dbChanges(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
